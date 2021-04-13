6af9c131a9199c493fd375fb4fa9a586a1d7ca08
FOX MASSAT <massat198@gmail.com>
2021-04-13 16:50:19 +0000

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;  
+
+
+namespace DevTechTuto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        } 
+
+        // declaration global pour la classe ado
+        ado d = new ado(); 
+        
+         // this is  for vider button 
+
+            public void vider(Control f )
+            {
+                foreach (Control ct in f.Controls)
+                { 
+                    // this for textbox
+                    if(ct.GetType()==typeof(TextBox))
+                    {
+                           ct.Text="";
+                    } 
+
+                    // this  for  group box  
+
+                    if(ct.Controls.Count!=0)
+                    {
+                        vider(ct);
+                    }
+                   
+                }
+
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            d.connecter();
+            d.cmd.CommandText = "select *from sta";
+            d.cmd.Connection = d.con; 
+
+            d.rd = d.cmd.ExecuteReader(); 
+            // other method using datatable 
+            d.dt.Load(d.rd);
+
+            dataGridView1.DataSource = d.dt;
+
+            d.rd.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("voulervous quitter ?","Confirmation",MessageBoxButtons.YesNo)==DialogResult.Yes) 
+                d.deconnecter();
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("vouler vous vider votre form","comfirmation",MessageBoxButtons.YesNo)==DialogResult.Yes)
+            {
+                vider(this); 
+            }
+        }
+        }
+    }
+
---
+++ ado.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data; 
+namespace DevTechTuto
+{
+    class ado
+    {  
+        // declaration des objects sql >>> mode connecte  
+
+        public SqlConnection con = new SqlConnection();
+        public SqlCommand cmd = new SqlCommand();
+        public SqlDataReader rd;
+        public DataTable dt = new DataTable(); 
+
+        // declaration de la methode connecte 
+           public void connecter()
+        {
+            if(con.State==ConnectionState.Closed ||con.State==ConnectionState.Broken)
+            {
+                con.ConnectionString = @"Data Source=DESKTOP-3GHSQJ4\SQLEXPRESS;Initial Catalog=adonet;Integrated Security=True";
+                con.Open();
+            }
+
+        }
+        // declaration de la metode  deconnecter  
+
+        public void deconnecter ()
+           {
+
+            if(con.State==ConnectionState.Open)
+            {
+                con.Close();
+            }
+           }
+
+    }
+}
